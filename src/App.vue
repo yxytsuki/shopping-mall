445722fd1b0ca431b56f3a5ecc77df0b8be9635c
yxytsuki <2751415916@qq.com>
2024-08-19 10:59:38 +0800

    App

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      checked: true
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.box {
+  width: 300px;
+  height: 300px;
+  background-color: pink;
+}
+
+</style>
